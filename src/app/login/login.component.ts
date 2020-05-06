94f806e7a2700f76f2ea7f3db29885098f86b45d
Gurkanwar <gurkanwarsingh@mail.fresnostate.edu>
2020-05-06 01:13:53 -0700

    finished exam

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,12 +3,24 @@ import { LocalStorageService } from '../localStorageService';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
 
+
+export interface IUser {
+  id?: number;
+  username: string;
+  password: string;
+}
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: IUser = {
+    username: null,
+    password: null
+  };
+
   constructor(private router: Router, private toastService: ToastService) {
   }
 
@@ -16,4 +28,27 @@ export class LoginComponent implements OnInit {
 
   }
 
+  login(user: IUser) {
+
+    const presetUser = { username: 'gurry', password: 'gurry789'};
+    if (user.username != null && user.password != null &&
+      user.username !== '' && user.password !== '')  {
+        // log the user in
+
+        if (user.username === presetUser.username &&
+          user.password === presetUser.password){
+            // actually log them in
+            // saving data to localStorage
+            localStorage.setItem('user', JSON.stringify(user));
+            // navigate to contacts page
+            this.router.navigate(['contacts', user]);
+
+
+          } else {
+            this.toastService.showToast('warning', 2000, 'Username or password is wrong!');
+          }
+
+    } else {
+      this.toastService.showToast('danger', 2000, 'Must specify credentials');
+  }
 }
